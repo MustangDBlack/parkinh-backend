8e878513c7626c9431bce2e1ff3bda2b5009cfc0
admin <mustangdblack@gmail.com>
2026-08-07 02:06:32 -0300

    Fix Spring Security CORS

--- src/main/java/com/example/parquimetro/security/SecurityConfig.java
+++ src/main/java/com/example/parquimetro/security/SecurityConfig.java
@@ -1,7 +1,8 @@
-package com.example.parquimetro.security;
+package com.blackkode.parkinh.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,11 +20,12 @@ public class SecurityConfig {
     // 2. Configuramos las puertas (Filtros)
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // Desactivamos CSRF para que React pueda enviar POSTs
-            .cors(cors -> cors.disable()) // Temporalmente desactivamos restricciones severas de origen
+        http
+            .csrf(csrf -> csrf.disable()) // Desactivamos CSRF para que React pueda enviar POSTs
+            .cors(Customizer.withDefaults()) // <-- CAMBIO CLAVE: Activa y enlaza tu configuración de CorsConfig.java
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/usuarios/registro").permitAll() // Puerta abierta para crear el primer Admin
-                .anyRequest().permitAll() // Dejamos el resto abierto temporalmente para no romper tu mapa visual
+                .anyRequest().permitAll() // Dejamos el resto abierto temporalmente
             );
         return http.build();
     }
