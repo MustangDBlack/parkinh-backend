45568834c9ffddecf0c2789e0094c350029bcdea
admin <mustangdblack@gmail.com>
2026-08-07 01:50:20 -0300

    Agregado CorsConfig para permitir conexion del frontend

---
+++ src/main/java/com/example/parquimetro/config/CorsConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas las rutas de tu API
+                        .allowedOrigins("https://parkinh.blackkode.com.ar") // Tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
